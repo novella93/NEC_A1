85451e3aa60f7b23335db967b7e2cc56bc0ac6fd
jnovella <javier.novella.ruiz.games@gmail.com>
2024-12-03 18:20:36 +0100

    Corrected bug to avoid NaN error

--- BP.py
+++ BP.py
@@ -8,7 +8,7 @@ def sigmoid_derivative(x):
     return x * (1 - x)
 
 class NeuralNet:
-    def __init__(self, layers, epochs, learning_speed, momentum):
+    def __init__(self, layers, epochs, learning_rate, momentum):
         ############################
         # ATTRIBUTE INITIALIZATION #
         ############################
@@ -19,8 +19,8 @@ class NeuralNet:
         self.n = layers.copy()
         # Epochs
         self.epochs = epochs
-        # Learning speed
-        self.learning_speed = learning_speed
+        # Learning rate
+        self.learning_rate = learning_rate
         # Momentum
         self.momentum = momentum
 
@@ -30,15 +30,15 @@ class NeuralNet:
         # Activations of the networks
         self.xi = [np.zeros((n, 1)) for n in layers]
 
-        # Weights
-        self.w = [np.random.randn(layers[i], layers[i - 1]) for i in range(1, self.L)]
+        # Weights (with smaller initialization)
+        self.w = [np.random.randn(layers[i], layers[i - 1]) * 0.01 for i in range(1, self.L)]
         # Weights changes
         self.d_w = [np.zeros((layers[i], layers[i - 1])) for i in range(1, self.L)]
         # Previous changes for weights
         self.d_w_prev = [np.zeros((layers[i], layers[i - 1])) for i in range(1, self.L)]
 
         # Thresholds
-        self.theta = [np.random.randn(n, 1) for n in layers[1:]]
+        self.theta = [np.random.randn(n, 1) * 0.01 for n in layers[1:]]
         # Thresholds changes
         self.d_theta = [np.zeros((n, 1)) for n in layers[1:]]
         # Previous changes for thresholds
@@ -52,14 +52,14 @@ class NeuralNet:
     #####################
 
     def forward_propagation(self, X):
-        activations = [X]
-        for i in range(len(self.w)):
-            X = sigmoid(np.dot(self.w[i], X) + self.theta[i])
-            activations.append(X)
-        return activations
+        self.xi[0] = X
+        for i in range(1, self.L):
+            self.h[i] = np.dot(self.w[i - 1], self.xi[i - 1]) - self.theta[i - 1]
+            self.xi[i] = sigmoid(self.h[i])
+        return self.xi
     
     def backward_propagation(self, y):
-        # Compute the gradient of the loss with respect the 
+        # Compute the gradient of the loss with respect to the output
         self.delta[-1] = (self.xi[-1] - y) * sigmoid_derivative(self.h[-1])
         for i in range(self.L - 2, 0, -1):
             self.delta[i] = np.dot(self.w[i].T, self.delta[i + 1]) * sigmoid_derivative(self.h[i])
@@ -67,9 +67,13 @@ class NeuralNet:
             self.d_w[i] = np.dot(self.delta[i + 1], self.xi[i].T)
             self.d_theta[i] = self.delta[i + 1]
 
+            # Gradient clipping
+            np.clip(self.d_w[i], -1, 1, out=self.d_w[i])
+            np.clip(self.d_theta[i], -1, 1, out=self.d_theta[i])
+
             # Update weights and thresholds with momentum
-            self.w[i] -= self.learning_speed * self.d_w[i] + self.momentum * self.d_w_prev[i]
-            self.theta[i] += self.learning_speed * self.d_theta[i] + self.momentum * self.d_theta_prev[i]
+            self.w[i] -= self.learning_rate * self.d_w[i] + self.momentum * self.d_w_prev[i]
+            self.theta[i] -= self.learning_rate * self.d_theta[i] + self.momentum * self.d_theta_prev[i]
 
             # Store current changes for the momentum term
             self.d_w_prev[i] = self.d_w[i]
@@ -86,8 +90,9 @@ class NeuralNet:
                 self.backward_propagation(y[:, j].reshape(-1, 1))
             # Monitor training process
             if epoch % 1000 == 0:
-                loss = np.mean((y - self.forward_propagation(X)[-1]) ** 2)
-                print('Epoch number: ' + str(epoch) + ', Loss value: ' + str(loss))
+                predictions = np.hstack([self.forward_propagation(X[:, j].reshape(-1, 1))[-1] for j in range(X.shape[1])])
+                loss = np.mean((y - predictions) ** 2)
+                print(f'Epoch number: {epoch}, Loss value: {loss}')
 
     def predict(self, X):
         return self.forward_propagation(X)[-1]
@@ -95,7 +100,7 @@ class NeuralNet:
 # Data preprocessing (scaling) 
 def scale_data(X, min_val, max_val): 
     return (X - min_val) / (max_val - min_val)
-    
+
 # Example usage
 if __name__ == "__main__":
     # Example data (X: inputs, y: targets)
@@ -106,7 +111,7 @@ if __name__ == "__main__":
     X_scaled = scale_data(X, X.min(), X.max())
 
     # Initialize and train the neural network
-    nn = NeuralNet(layers=[2, 3, 1], epochs=10000, learning_speed=0.1, momentum=0.9)
+    nn = NeuralNet(layers=[2, 3, 1], epochs=10000, learning_rate=0.9, momentum=0.9)
     nn.train(X_scaled, y)
 
     # Make predictions
